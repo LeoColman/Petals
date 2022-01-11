38eb3588c6a11777a4e9e7f99f03a937ce69fe6f
Leonardo Colman Lopes <leonardo.dev@colman.com.br>
2022-01-11 16:52:56 -0300

    Add HitTimer "time's up" effect
    Co-authored-by: Luiz Neves Porto <dev@luiz.nevesporto.com.br>

--- app/src/main/kotlin/br/com/colman/petals/pages/HitTimer.kt
+++ app/src/main/kotlin/br/com/colman/petals/pages/HitTimer.kt
@@ -18,19 +18,15 @@
 
 package br.com.colman.petals.pages
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,12 +40,14 @@ import java.time.LocalDateTime
 import java.time.LocalDateTime.now
 import java.time.temporal.ChronoUnit.MILLIS
 
+private const val tenSecondsMillis = 10_000L
+
 @Preview
 @Composable
 fun ComposeHitTimer() {
   val hitTimer = remember { HitTimer() }
 
-  val millisLeft by hitTimer.millisLeft.collectAsState(10_000L)
+  val millisLeft by hitTimer.millisLeft.collectAsState(initial = tenSecondsMillis)
 
   Column(Modifier.fillMaxWidth().padding(top = 60.dp), spacedBy(24.dp), CenterHorizontally) {
     TimerText(millisLeft)
@@ -68,8 +66,31 @@ fun ComposeHitTimer() {
 
 @Composable
 private fun TimerText(millisLeft: Long) {
+  val isTimerRunning = millisLeft > 0L
+  var blinking by remember { mutableStateOf(false) }
+
+  LaunchedEffect(isTimerRunning) {
+    if(isTimerRunning) { blinking = false; return@LaunchedEffect }
+
+    repeat(7) {
+      blinking = !blinking
+      delay(150)
+    }
+  }
+
   val duration = HitTimer.duration(millisLeft)
-  Text(duration, fontSize = 52.sp)
+
+  if (blinking) BlinkingText(duration) else NonBlinkingText(duration)
+}
+
+@Composable
+private fun BlinkingText(text: String) {
+  Text(text, fontSize = 60.sp, color = Red, modifier = Modifier.height(100.dp))
+}
+
+@Composable
+private fun NonBlinkingText(text: String) {
+  Text(text, fontSize = 52.sp, color = Black, modifier = Modifier.height(100.dp))
 }
 
 private class HitTimer {
@@ -85,25 +106,20 @@ private class HitTimer {
   }
 
   val millisLeft = flow {
-    while(true) {
+    while (true) {
       delay(13)
       emit(calculateMillisLeft())
-      println(calculateMillisLeft())
     }
   }
 
   private fun calculateMillisLeft(): Long {
-    println(startDate)
     return startDate?.let { startDate ->
       val millisElapsed = startDate.until(now(), MILLIS)
       (tenSecondsMillis - millisElapsed).coerceAtLeast(0)
     } ?: tenSecondsMillis
   }
 
-  private val tenSecondsMillis = 10_000L
-
   companion object {
-    fun duration(millis: Long) = formatDuration(millis, "ss:SSS")
+    fun duration(millis: Long): String = formatDuration(millis, "ss:SSS")
   }
 }
-
