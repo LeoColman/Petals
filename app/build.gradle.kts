29234b4d5081b00d0245c9c45cebbeaaf76703fb
LeoColman <dev@leonardo.colman.com.br>
2022-03-01 17:08:11 -0300

    Release v2.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
     applicationId = "br.com.colman.petals"
     minSdk = 21
     targetSdk = 30
-    versionCode = 201
-    versionName = "v2.0.1"
+    versionCode = 202
+    versionName = "v2.0.2"
 
     testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
   }
