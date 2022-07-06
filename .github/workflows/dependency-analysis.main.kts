3c75f3ed4f7c9aae190aace21f9a8d098f54bcc4
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-07-06 18:54:50 -0300

    👷 Migrate dependency analysis to use github-actions-kotlin

---
+++ .github/workflows/dependency-analysis.main.kts
@@ -0,0 +1,25 @@
+#!/usr/bin/env kotlin
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.21.0")
+
+import it.krzeminski.githubactions.actions.actions.CheckoutV3
+import it.krzeminski.githubactions.actions.actions.UploadArtifactV3
+import it.krzeminski.githubactions.actions.gradle.GradleBuildActionV2
+import it.krzeminski.githubactions.domain.RunnerType
+import it.krzeminski.githubactions.domain.triggers.Push
+import it.krzeminski.githubactions.dsl.workflow
+import it.krzeminski.githubactions.yaml.writeToFile
+
+workflow(
+  name = "Dependency Analysis",
+  on = listOf(Push()),
+  sourceFile = __FILE__.toPath()
+) {
+  job("analyse", runsOn = RunnerType.UbuntuLatest) {
+    uses(CheckoutV3())
+    uses(GradleBuildActionV2(arguments = "buildHealth"))
+    uses(UploadArtifactV3(
+      name = "dependency-analysis-output",
+      path = listOf("build/reports/dependency-analysis/build-health-report.txt")
+    ))
+  }
+}.writeToFile()
