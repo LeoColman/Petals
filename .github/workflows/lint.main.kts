8a71cbd7b8f8c1f2c1372ea5dc52e49a13cf08d0
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-07-06 17:42:56 -0300

    👷 Converts Lint to use github-actions-kotlin

---
+++ .github/workflows/lint.main.kts
@@ -0,0 +1,20 @@
+#!/usr/bin/env kotlin
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.21.0")
+
+import it.krzeminski.githubactions.actions.actions.CheckoutV3
+import it.krzeminski.githubactions.actions.gradle.GradleBuildActionV2
+import it.krzeminski.githubactions.domain.RunnerType.UbuntuLatest
+import it.krzeminski.githubactions.domain.triggers.Push
+import it.krzeminski.githubactions.dsl.workflow
+import it.krzeminski.githubactions.yaml.writeToFile
+
+workflow(
+  name = "Lint",
+  on = listOf(Push()),
+  sourceFile = __FILE__.toPath()
+) {
+  job("detekt", runsOn = UbuntuLatest) {
+    uses(CheckoutV3())
+    uses(GradleBuildActionV2(arguments = "detekt"))
+  }
+}.writeToFile()
