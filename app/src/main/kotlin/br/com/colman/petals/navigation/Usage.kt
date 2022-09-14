d42e2096771ff0871f94d7801d9fb6742be68107
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-09-14 18:38:27 -0300

    💄 Add Pause Button to Usage section
    First subtask of #86
    
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- app/src/main/kotlin/br/com/colman/petals/navigation/Usage.kt
+++ app/src/main/kotlin/br/com/colman/petals/navigation/Usage.kt
@@ -20,18 +20,23 @@ package br.com.colman.petals.navigation
 
 import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import br.com.colman.petals.use.AddUseButton
 import br.com.colman.petals.use.LastUseDateTimer
 import br.com.colman.petals.use.StatsBlocks
 import br.com.colman.petals.use.UseCards
+import br.com.colman.petals.use.pause.PauseButton
 import br.com.colman.petals.use.repository.UseRepository
 import org.koin.androidx.compose.get
 
@@ -42,7 +47,10 @@ fun Usage(useRepository: UseRepository = get()) {
   Column(Modifier.verticalScroll(rememberScrollState()), spacedBy(8.dp), CenterHorizontally) {
     lastUseDate?.let { LastUseDateTimer(it) }
 
-    AddUseButton(useRepository)
+    Row(Modifier.padding(8.dp), spacedBy(8.dp), CenterVertically) {
+      AddUseButton(useRepository)
+      PauseButton()
+    }
 
     val uses by useRepository.all().collectAsState(emptyList())
     if (uses.isNotEmpty()) {
