0a73cd39531e2b03ae488e60a245139a5ff42e38
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-09-14 18:38:27 -0300

    💄 Add Pause Button to Usage section
    First subtask of #86
    
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

---
+++ app/src/main/kotlin/br/com/colman/petals/use/pause/PauseButton.kt
@@ -0,0 +1,25 @@
+package br.com.colman.petals.use.pause
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import compose.icons.TablerIcons
+import compose.icons.tablericons.Alarm
+
+@Preview
+@Composable
+fun PauseButton() {
+  Row(Modifier.padding(8.dp).clickable {  }, spacedBy(8.dp), CenterVertically) {
+    Icon(TablerIcons.Alarm, null)
+    Text("Pause Time")
+  }
+}
+
