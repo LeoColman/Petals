26c4dfec64b357a69c3482f0f2644d9c6bbae931
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-10-07 11:04:10 -0300

    ✨ Add Confirmation to Add Use during pause
    The Pause Dialog will open after clicking the button placed near `Add
    Use`.
    
    This commit implements confirmation dialog before going to the add use
    screen
    
    Subtask of #86
    
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- app/src/main/kotlin/br/com/colman/petals/use/AddUseButton.kt
+++ app/src/main/kotlin/br/com/colman/petals/use/AddUseButton.kt
@@ -1,34 +1,112 @@
 package br.com.colman.petals.use
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.colman.petals.R.string.add_use
+import br.com.colman.petals.R.string.no
 import br.com.colman.petals.R.string.ok
+import br.com.colman.petals.R.string.yes
+import br.com.colman.petals.R.string.yes_timer
+import br.com.colman.petals.use.pause.repository.Pause
 import br.com.colman.petals.use.repository.Use
 import br.com.colman.petals.use.repository.UseRepository
+import compose.icons.TablerIcons
+import compose.icons.tablericons.Lock
+import kotlinx.coroutines.delay
 import java.math.BigDecimal.ZERO
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 
 @Composable
-fun AddUseButton(repository: UseRepository) {
-  var openDialog by remember { mutableStateOf(false) }
+fun AddUseButton(
+  repository: UseRepository,
+  pause: Pause? = null
+) {
+  var openAddUseDialog by remember { mutableStateOf(false) }
+  var openConfirmAddUseDialog by remember { mutableStateOf(false) }
   val lastUse by repository.getLastUse().collectAsState(null)
 
-  if (openDialog) {
-    AddUseDialog(lastUse, { repository.insert(it) }) { openDialog = false }
+  if (openAddUseDialog) {
+    AddUseDialog(lastUse, { repository.insert(it) }) { openAddUseDialog = false }
   }
 
-  Button(onClick = { openDialog = true }) {
+  if (openConfirmAddUseDialog) {
+    ConfirmAddUseDuringPauseDialog({ openConfirmAddUseDialog = false }) {
+      openConfirmAddUseDialog = false
+      openAddUseDialog = true
+    }
+  }
+
+
+  if (pause == null) {
+    UseButton { openAddUseDialog = true }
+  } else {
+    PauseUseButton { openConfirmAddUseDialog = true }
+  }
+}
+
+@Preview
+@Composable
+private fun UseButton(onClick: () -> Unit = { }) {
+  Button(onClick) {
     Text(stringResource(add_use))
   }
 }
+
+@Preview
+@Composable
+private fun PauseUseButton(onClick: () -> Unit = { }) {
+  Button(onClick) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+      Text(stringResource(add_use))
+      Icon(TablerIcons.Lock, contentDescription = null)
+    }
+  }
+}
+
+@Composable
+@Preview
+private fun ConfirmAddUseDuringPauseDialog(
+  onDismiss: () -> Unit = {},
+  onConfirm: () -> Unit = {}
+) {
+
+  var yesTimer by remember { mutableStateOf(10) }
+  var yesEnabled by remember { mutableStateOf(yesTimer == 0) }
+
+  LaunchedEffect(yesTimer) {
+    while (yesTimer > 0) {
+      delay(1000)
+      yesTimer--
+      yesEnabled = yesTimer == 0
+    }
+  }
+
+  AlertDialog(
+    onDismissRequest = onDismiss,
+    text = { Text("You scheduled this period to be a pause. Are you sure you want to add an use now?") },
+    confirmButton = {
+      TextButton(onConfirm, enabled = yesEnabled) {
+        if(yesEnabled) {
+          Text(stringResource(yes))
+        } else {
+          Text(stringResource(yes_timer, yesTimer))
+        }
+      }
+    },
+    dismissButton = { TextButton(onDismiss) { Text(stringResource(no)) } }
+  )
+}
+
 
 @Suppress("NAME_SHADOWING")
 @Composable
