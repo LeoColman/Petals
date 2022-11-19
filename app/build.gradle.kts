2661d1ddf1dbf3c230b25a1f86dcd6b03ccad334
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-11-19 15:18:25 -0500

    🔖 Prepare release 3.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
     applicationId = "br.com.colman.petals"
     minSdk = 21
     targetSdk = 33
-    versionCode = 3200
-    versionName = "3.2.0"
+    versionCode = 3201
+    versionName = "3.2.1"
 
     testApplicationId = "$applicationId.test"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
