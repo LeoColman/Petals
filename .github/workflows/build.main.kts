8743b03d11c9a340bb9533c67f4b412f84041732
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-12-22 21:27:11 -0500

    👷 Add Build APK workflow
    Closes #174

---
+++ .github/workflows/build.main.kts
@@ -0,0 +1,41 @@
+#!/usr/bin/env kotlin
+@file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.33.0")
+
+import it.krzeminski.githubactions.actions.CustomAction
+import it.krzeminski.githubactions.actions.actions.CheckoutV3
+import it.krzeminski.githubactions.actions.actions.SetupJavaV3
+import it.krzeminski.githubactions.actions.gradle.GradleBuildActionV2
+import it.krzeminski.githubactions.domain.RunnerType
+import it.krzeminski.githubactions.domain.triggers.PullRequest
+import it.krzeminski.githubactions.domain.triggers.Push
+import it.krzeminski.githubactions.dsl.expressions.Contexts
+import it.krzeminski.githubactions.dsl.expressions.expr
+import it.krzeminski.githubactions.dsl.workflow
+import it.krzeminski.githubactions.yaml.writeToFile
+
+val GPG_KEY by Contexts.secrets
+
+workflow(
+  name = "Build Universal APK",
+  on = listOf(Push(), PullRequest()),
+  sourceFile = __FILE__.toPath()
+) {
+  job("build", runsOn = RunnerType.UbuntuLatest) {
+    uses(name = "Set up JDK", SetupJavaV3("11", SetupJavaV3.Distribution.Adopt))
+    uses(CheckoutV3())
+    uses(
+      "reveal-secrets", CustomAction(
+        "entrostat",
+        "git-secret-action",
+        "v3.3.0",
+        mapOf("gpg-private-key" to expr { GPG_KEY })
+      )
+    )
+
+    uses(
+      "Create APK", GradleBuildActionV2(
+        arguments = "packageGithubReleaseUniversalApk"
+      )
+    )
+  }
+}.writeToFile()
