07d36b39cf7e5d0d7ae1f7fd65d16fae2010f322
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-01-27 21:29:52 -0500

    ✅ Fix HitTimerTest failing when it runs too quickly

--- app/src/test/kotlin/br/com/colman/petals/hittimer/HitTimerTest.kt
+++ app/src/test/kotlin/br/com/colman/petals/hittimer/HitTimerTest.kt
@@ -2,8 +2,7 @@ package br.com.colman.petals.hittimer
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.inspectors.forAll
-import io.kotest.matchers.collections.shouldEndWith
-import io.kotest.matchers.collections.shouldStartWith
+import io.kotest.matchers.collections.shouldBeMonotonicallyDecreasing
 import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.flow.take
@@ -24,8 +23,8 @@ class HitTimerTest : FunSpec({
     val flow = target.millisLeft
     target.start()
     val allResults = flow.take(300).toList()
-    allResults shouldStartWith 100
-    allResults shouldEndWith 0
+    allResults.shouldBeMonotonicallyDecreasing()
+    allResults.last() shouldBe 0
   }
 
   test("Should emit at least 10% of values in duration") {
