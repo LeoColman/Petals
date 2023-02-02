d43bbb31174cdb7bf36b2d00042ecb0041967820
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-02-02 17:50:26 -0500

    🥚Add small effect when last use time is exactly 16:20
    https://en.wikipedia.org/wiki/420_(cannabis_culture)
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- app/src/main/kotlin/br/com/colman/petals/use/LastUseDateTimer.kt
+++ app/src/main/kotlin/br/com/colman/petals/use/LastUseDateTimer.kt
@@ -19,25 +19,50 @@
 package br.com.colman.petals.use
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import br.com.colman.petals.R.string.*
+import br.com.colman.petals.R.string.days
+import br.com.colman.petals.R.string.hours
+import br.com.colman.petals.R.string.milliseconds
+import br.com.colman.petals.R.string.minutes
+import br.com.colman.petals.R.string.months
+import br.com.colman.petals.R.string.quit_date_text
+import br.com.colman.petals.R.string.seconds
+import br.com.colman.petals.R.string.years
 import br.com.colman.petals.settings.SettingsRepository
-import br.com.colman.petals.use.TimeUnit.*
+import br.com.colman.petals.use.TimeUnit.Day
+import br.com.colman.petals.use.TimeUnit.Hour
+import br.com.colman.petals.use.TimeUnit.Millisecond
+import br.com.colman.petals.use.TimeUnit.Minute
+import br.com.colman.petals.use.TimeUnit.Month
+import br.com.colman.petals.use.TimeUnit.Second
+import br.com.colman.petals.use.TimeUnit.Year
+import br.com.colman.petals.utils.truncatedToMinute
 import kotlinx.coroutines.delay
 import org.koin.androidx.compose.get
 import java.time.LocalDateTime
 import java.time.LocalDateTime.now
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
-import java.util.*
+import java.util.Locale
 
 @Composable
 fun LastUseDateTimer(lastUseDate: LocalDateTime) {
@@ -72,7 +97,8 @@ fun LastUseDateTimer(lastUseDate: LocalDateTime) {
   Column(Modifier.padding(16.dp), Arrangement.spacedBy(16.dp)) {
     Column {
       Text(stringResource(quit_date_text))
-      Text(dateString, fontSize = 24.sp)
+      val dateStringWithExtras = if (!lastUseDate.is420()) dateString else "$dateString 🥦🥦"
+      Text(dateStringWithExtras, fontSize = 24.sp)
     }
 
     Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
@@ -104,3 +130,5 @@ private enum class TimeUnit(@StringRes val unitName: Int, val max: Long, val mil
   Month(months, 12L, Day.max * Day.millis),
   Year(years, 60L, Month.max * Month.millis),
 }
+
+private fun LocalDateTime.is420() = toLocalTime().truncatedToMinute() == LocalTime.of(16, 20)
