c8aa060b32dcd69c432418a56f1b010282af4188
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-02-11 13:31:38 -0500

    👷 Make Build CI not depend on secrets
    External pull requests don't need to depend on secrets to run an APK
    build. this commit makes our pipeline think the same way.
    
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- .github/workflows/build.main.kts
+++ .github/workflows/build.main.kts
@@ -1,7 +1,6 @@
 #!/usr/bin/env kotlin
 @file:DependsOn("it.krzeminski:github-actions-kotlin-dsl:0.35.0")
 
-import it.krzeminski.githubactions.actions.CustomAction
 import it.krzeminski.githubactions.actions.actions.CheckoutV3
 import it.krzeminski.githubactions.actions.actions.SetupJavaV3
 import it.krzeminski.githubactions.actions.gradle.GradleBuildActionV2
@@ -9,7 +8,6 @@ import it.krzeminski.githubactions.domain.RunnerType
 import it.krzeminski.githubactions.domain.triggers.PullRequest
 import it.krzeminski.githubactions.domain.triggers.Push
 import it.krzeminski.githubactions.dsl.expressions.Contexts
-import it.krzeminski.githubactions.dsl.expressions.expr
 import it.krzeminski.githubactions.dsl.workflow
 import it.krzeminski.githubactions.yaml.writeToFile
 
@@ -23,18 +21,9 @@ workflow(
   job("build", runsOn = RunnerType.UbuntuLatest) {
     uses(name = "Set up JDK", SetupJavaV3("11", distribution = SetupJavaV3.Distribution.Adopt))
     uses(CheckoutV3())
-    uses(
-      "reveal-secrets", CustomAction(
-        "entrostat",
-        "git-secret-action",
-        "v3.3.0",
-        mapOf("gpg-private-key" to expr { GPG_KEY })
-      )
-    )
-
-    uses(
+        uses(
       "Create APK", GradleBuildActionV2(
-        arguments = "packageGithubReleaseUniversalApk"
+        arguments = "packageFdroidReleaseUniversalApk"
       )
     )
   }
