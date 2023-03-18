866eb67c76687ae3c73a7eb8ba3656844c3e346a
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-03-17 21:30:34 -0400

    🚧 Add initial implementation for PIN authorization
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- app/src/main/kotlin/br/com/colman/petals/MainActivity.kt
+++ app/src/main/kotlin/br/com/colman/petals/MainActivity.kt
@@ -22,35 +22,83 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.rememberNavController
+import br.com.colman.petals.R.string.pin_main_screen
 import br.com.colman.petals.navigation.BottomNavigationBar
 import br.com.colman.petals.navigation.MyTopAppBar
 import br.com.colman.petals.navigation.NavHostContainer
+import java.time.LocalDateTime
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Suppress("FunctionName")
 class MainActivity : ComponentActivity(), CoroutineScope by CoroutineScope(Dispatchers.Main) {
+
+  private var authorizedUntil = LocalDateTime.MIN
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
       val navController = rememberNavController()
 
-      MaterialTheme(if (isSystemInDarkTheme()) darkColors() else lightColors()) {
-        Surface {
-          Scaffold(
-            topBar = { MyTopAppBar(navController) },
-            bottomBar = { BottomNavigationBar(navController) },
-            content = { NavHostContainer(navController) }
-          )
+      var isAuthorized by remember { mutableStateOf(false) }
+
+      LaunchedEffect(authorizedUntil) {
+        while (true) {
+          isAuthorized = isAuthorized()
+          delay(1000)
         }
       }
+
+      MaterialTheme(if (isSystemInDarkTheme()) darkColors() else lightColors()) {
+        if (isAuthorized) {
+          Surface {
+            Scaffold(
+              topBar = { MyTopAppBar(navController) },
+              bottomBar = { BottomNavigationBar(navController) },
+              content = { NavHostContainer(navController) }
+            )
+          }
+        } else {
+          Authorization()
+        }
+      }
+    }
+  }
+
+  fun isAuthorized() = authorizedUntil >= LocalDateTime.now()
+
+  @Composable
+  fun Authorization() {
+    var pin by remember { mutableStateOf("") }
+
+    LaunchedEffect(pin) {
+      if (pin == "abcd") {
+        authorizedUntil = LocalDateTime.now().plusMinutes(30L)
+      }
+    }
+
+    Column {
+      Text(stringResource(pin_main_screen))
+      OutlinedTextField(pin, { pin = it })
     }
   }
 }
