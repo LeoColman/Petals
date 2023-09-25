31ad695fe63973d83daac4c03b40d74d442273cd
YellowRoseCx <80486540+YellowRoseCx@users.noreply.github.com>
2023-09-25 18:09:28 -0500

    Add date to exported fileName
    This changes the hardcoded "PetalsExport.csv" fileName to a unique one that identifies the date it was saved
    
    Example: "PetalsExport-2023-09-25.csv"

--- app/src/main/kotlin/br/com/colman/petals/use/io/FileWriter.kt
+++ app/src/main/kotlin/br/com/colman/petals/use/io/FileWriter.kt
@@ -5,11 +5,14 @@ import android.net.Uri
 import androidx.core.content.FileProvider.getUriForFile
 import br.com.colman.petals.BuildConfig.APPLICATION_ID
 import java.io.File
+import java.time.LocalDate
 
 class FileWriter(private val context: Context) {
 
-  private val fileName = "PetalsExport.csv"
-
+  private fun generateFileName(): String {
+   val date = LocalDate.now()
+   return "PetalsExport-$date.csv"
+  }
   private val exportsDir by lazy {
     File(context.filesDir, "exports").apply {
       if (!exists()) mkdirs()
@@ -22,5 +25,5 @@ class FileWriter(private val context: Context) {
     return getUriForFile(context, APPLICATION_ID, exportedFile)
   }
 
-  private fun createFile(content: String) = File(exportsDir, fileName).apply { writeText(content) }
+  private fun createFile(content: String) = File(exportsDir, generateFileName()).apply { writeText(content) }
 }
