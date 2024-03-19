22b6b257587bfad36293590988f3d2ae06bf41d9
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2024-03-19 18:20:48 -0300

    💄 Refactor label generation in graph classes
    Unused imports have been removed and label generation in UsePerHourGraph
     and UsePerDayOfWeekGraph has been changed. Now, instead of using a
     pluralStringResource, the label method from Period is used, simplifying
      the code and centralizing label generation.
    
      Closes #521
    
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- app/src/main/kotlin/br/com/colman/petals/statistics/graph/UsePerDayOfWeekGraph.kt
+++ app/src/main/kotlin/br/com/colman/petals/statistics/graph/UsePerDayOfWeekGraph.kt
@@ -2,10 +2,8 @@ package br.com.colman.petals.statistics.graph
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import br.com.colman.petals.R
 import br.com.colman.petals.R.string
 import br.com.colman.petals.statistics.component.Period
 import br.com.colman.petals.statistics.graph.component.LineChart
@@ -56,7 +54,7 @@ fun UsePerDayOfWeekGraph(useGroups: Map<Period, List<Use>>) {
   val description = stringResource(string.grams_distribution_per_day_of_week)
   val colors = MaterialTheme.colors
   val gramsData = useGroups.map { (period, uses) ->
-    val label = pluralStringResource(R.plurals.last_x_days, period.days, period.days)
+    val label = period.label()
     createDistributionPerDayOfWeekDataset(period.days, uses, label, colors)
   }
 
