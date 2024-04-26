a99595575eb8173a991dce1654f4d78ad6aec0b2
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2024-04-26 19:29:41 -0300

    💄 Refactor Period class and enhance data handling in UsePerDayOfWeekGraph
    Converted the Period class from sealed to open and added a 'minusDays'
    method. Made modifications in the UsePerDayOfWeekGraph class to include
    an additional 'uses3' dataset and optimized the data grouping and
    filtering logic to improve weekly data management.
    
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- app/src/main/kotlin/br/com/colman/petals/statistics/graph/color/Colors.kt
+++ app/src/main/kotlin/br/com/colman/petals/statistics/graph/color/Colors.kt
@@ -6,8 +6,11 @@ val colors = mapOf(
   0 to Color.Green,
   7 to Color.Blue,
   14 to Color.Yellow,
+  28 to Color.Red,
   30 to Color.Red,
+  56 to Color.Gray,
   60 to Color.Gray,
+  84 to Color.DarkGray,
   90 to Color.DarkGray
 ).withDefault { Color.Red }
 
