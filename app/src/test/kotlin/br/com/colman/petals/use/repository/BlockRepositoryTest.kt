d7eadb377096565adf9ae419a1f928e370bfac95
Gabriel Rodrigues <103524748+gBL17@users.noreply.github.com>
2024-05-21 18:57:29 -0300

    ✅ Add Unit Tests for BlockRepository DataStore (#577)
    Co-authored-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

---
+++ app/src/test/kotlin/br/com/colman/petals/use/repository/BlockRepositoryTest.kt
@@ -0,0 +1,37 @@
+package br.com.colman.petals.use.repository
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.engine.spec.tempfile
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.flow.first
+
+class BlockRepositoryTest : FunSpec({
+  val datastore: DataStore<Preferences> = PreferenceDataStoreFactory.create { tempfile(suffix = ".preferences_pb") }
+  val target = BlockRepository(datastore)
+
+  test("Defaults block censor true") {
+    BlockType.entries.forEach { blockType ->
+      val isCensored = when (blockType) {
+        BlockType.Today -> target.isTodayCensored.first()
+        BlockType.ThisWeek -> target.isThisWeekCensored.first()
+        BlockType.ThisMonth -> target.isThisMonthCensored.first()
+        BlockType.ThisYear -> target.isThisYearCensored.first()
+        BlockType.AllTime -> target.isAllTimeCensored.first()
+      }
+      isCensored shouldBe true
+    }
+  }
+
+  test("Changes today's block censure") {
+    target.setBlockCensure(BlockType.Today, false)
+    target.isTodayCensored.first() shouldBe false
+  }
+
+  test("Persists specified block censure") {
+    target.setBlockCensure(BlockType.Today, false)
+    datastore.data.first()[BlockType.Today.preferencesKey] shouldBe false
+  }
+})
