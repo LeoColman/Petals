f7b79153a14e809ef547d3844091aa6f2bda7b27
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2024-09-07 22:33:34 -0300

    ✏️ Undo accidental renaming of ClockFormatEnum
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- app/src/main/kotlin/br/com/colman/petals/utils/datetime/ClockFormatEnum.kt
+++ app/src/main/kotlin/br/com/colman/petals/utils/datetime/ClockFormatEnum.kt
@@ -3,7 +3,7 @@ package br.com.colman.petals.utils.datetime
 import androidx.annotation.StringRes
 import br.com.colman.petals.R
 
-enum class ClockFormat(@StringRes val nameRes: Int) {
+enum class ClockFormatEnum(@StringRes val nameRes: Int) {
   Hours12(R.string.hours_12),
   Hours24(R.string.hours_24)
 }
