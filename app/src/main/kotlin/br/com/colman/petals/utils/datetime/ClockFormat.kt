1a93f9f7b6d5ef6137faad6df1ac065b6fcc4f75
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2024-09-07 22:32:53 -0300

    🎨 Rename Constants from ClockFormatEnum
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

---
+++ app/src/main/kotlin/br/com/colman/petals/utils/datetime/ClockFormat.kt
@@ -0,0 +1,9 @@
+package br.com.colman.petals.utils.datetime
+
+import androidx.annotation.StringRes
+import br.com.colman.petals.R
+
+enum class ClockFormat(@StringRes val nameRes: Int) {
+  Hours12(R.string.hours_12),
+  Hours24(R.string.hours_24)
+}
