6fd6750f2cdef78442a1080ca97efee83958b3c0
= <=>
2024-09-16 13:37:50 +0530

    fixing unit test error

--- app/src/playstore/kotlin/br/com/colman/petals/playstore/KoinModules.kt
+++ app/src/playstore/kotlin/br/com/colman/petals/playstore/KoinModules.kt
@@ -1,7 +1,9 @@
 package br.com.colman.petals.playstore
 
 import android.content.Context
+import androidx.datastore.preferences.preferencesDataStore
 import br.com.colman.petals.playstore.inapp.InAppPurchase
+import br.com.colman.petals.playstore.settings.AdsSettingsRepository
 import org.koin.dsl.module
 
 private val Context.adsSettings by preferencesDataStore("ads_settings")
@@ -11,7 +13,7 @@ val KoinModule = module {
     InAppPurchase(get<Context>())
   }
   single {
-    AdsSettingsRepository(get<Context>().adSettings)
+    AdsSettingsRepository(get<Context>().adsSettings)
   }
 }
 
