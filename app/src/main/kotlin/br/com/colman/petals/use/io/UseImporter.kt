78c087fce1ae26854d45a38a22735608cddf744c
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2024-09-16 13:40:21 -0300

    Discard changes to app/src/main/kotlin/br/com/colman/petals/use/io/UseImporter.kt

--- app/src/main/kotlin/br/com/colman/petals/use/io/UseImporter.kt
+++ app/src/main/kotlin/br/com/colman/petals/use/io/UseImporter.kt
@@ -25,14 +25,13 @@ class UseImporter(
   private val useRepository: UseRepository
 ) {
 
-  fun import(csvFileLines: List<String>, modifyUse: (Use) -> (Use) = { it }): Result<Unit> =
-    runCatching {
-      val uses = csvFileLines.mapIndexed { index, s ->
-        UseCsvParser.parse(s).onFailure {
-          if (index > 0) throw it
-        }
-      }.mapNotNull { it.getOrNull() }
+  fun import(csvFileLines: List<String>, modifyUse: (Use) -> (Use) = { it }): Result<Unit> = runCatching {
+    val uses = csvFileLines.mapIndexed { index, s ->
+      UseCsvParser.parse(s).onFailure {
+        if (index > 0) throw it
+      }
+    }.mapNotNull { it.getOrNull() }
 
-      useRepository.upsertAll(uses.map(modifyUse))
-    }
+    useRepository.upsertAll(uses.map(modifyUse))
+  }
 }
