62aa4729d90b02ec16f390a4287295be41e11eac
gBL17 <fgrodrigues.gabriel@gmail.com>
2024-09-18 17:49:42 -0300

    :rotating_light: enhance all-time graph logic and add unit test
    - Refactor 'calculateGramsDistributionPerDaySinceFirstUse' function for
      better readability and accuracy
    - Add unit test for 'createLineDataSet' function to ensure correctness
    
    Refactored 'AllTimeGraph' and related components to use the newly
    modularized logic.
    
    # Conflicts:
    #	app/src/main/res/values-pt/strings.xml

--- app/src/main/kotlin/br/com/colman/petals/statistics/graph/formatter/DaysSinceFirstUseFormatter.kt
+++ app/src/main/kotlin/br/com/colman/petals/statistics/graph/formatter/DaysSinceFirstUseFormatter.kt
@@ -1,8 +1,13 @@
 package br.com.colman.petals.statistics.graph.formatter
 
+import br.com.colman.petals.koin
+import br.com.colman.petals.settings.SettingsRepository
 import br.com.colman.petals.use.repository.Use
+import br.com.colman.petals.use.repository.UseRepository
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.formatter.IAxisValueFormatter
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
