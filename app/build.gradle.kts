5731095dcf43dc7b155ad6daa50a3b5703aec384
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2024-09-18 20:52:13 -0300

    🔖 Prepare release 3.29.0
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
     applicationId = "br.com.colman.petals"
     minSdk = 21
     targetSdk = 34
-    versionCode = 328005
-    versionName = "3.28.1"
+    versionCode = 329000
+    versionName = "3.29.0"
 
     testApplicationId = "$applicationId.test"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
