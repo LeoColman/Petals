b6ef3d9c635126c490e37eeb45b49c5c122004ff
Rishabh Jain <68217946+rishabh-jain-1990@users.noreply.github.com>
2024-10-10 19:33:25 +0530

    ✨ Change 60 and 90 days stats line color (#734)

--- app/src/main/kotlin/br/com/colman/petals/statistics/graph/color/Colors.kt
+++ app/src/main/kotlin/br/com/colman/petals/statistics/graph/color/Colors.kt
@@ -8,10 +8,10 @@ val colors = mapOf(
   14 to Color.Yellow,
   28 to Color.Red,
   30 to Color.Red,
-  56 to Color.Gray,
-  60 to Color.Gray,
-  84 to Color.DarkGray,
-  90 to Color.DarkGray
+  56 to Color(0xFF4DD0E1), // Cyan
+  60 to Color(0xFF4DD0E1), // Cyan
+  84 to Color(0XFFFF8A65), // Orange
+  90 to Color(0XFFFF8A65), // Orange
 ).withDefault { Color.Red }
 
 fun createColor(days: Int): Color {
