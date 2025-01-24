888a8bd84553263118df7031dd5b7ad09c7a5172
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2025-01-24 04:56:56 -0300

    👷 Add Kover metrics to CI
    Closes #198
    
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

---
+++ .github/workflows/kover.main.kts
@@ -0,0 +1,30 @@
+#!/usr/bin/env kotlin
+@file:Repository("https://repo1.maven.org/maven2/")
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:3.2.0")
+
+@file:Repository("https://bindings.krzeminski.it")
+@file:DependsOn("actions:checkout:v4")
+@file:DependsOn("actions:setup-java:v4")
+@file:DependsOn("gradle:gradle-build-action:v3")
+
+import io.github.typesafegithub.workflows.actions.actions.Checkout
+import io.github.typesafegithub.workflows.actions.actions.SetupJava
+import io.github.typesafegithub.workflows.actions.gradle.GradleBuildAction
+import io.github.typesafegithub.workflows.domain.RunnerType
+import io.github.typesafegithub.workflows.domain.triggers.PullRequest
+import io.github.typesafegithub.workflows.domain.triggers.Push
+import io.github.typesafegithub.workflows.dsl.workflow
+
+
+workflow(
+  name = "Dependency License Analysis",
+  on = listOf(Push(branches = listOf("main"))),
+  sourceFile = __FILE__
+) {
+  job(id = "analyse", runsOn = RunnerType.UbuntuLatest) {
+    uses(name = "Set up JDK", action = SetupJava(javaVersion = "17", distribution = SetupJava.Distribution.Adopt))
+    uses(action = Checkout())
+    uses(action = GradleBuildAction(arguments = "koverHtmlReport"))
+    run(command = "cat app/build/reports/kover/html/index.html >> \$GITHUB_STEP_SUMMARY")
+  }
+}
