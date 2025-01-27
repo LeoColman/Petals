0055d180759ef210cd79cd3be8d67dbf43effd99
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2025-01-27 18:56:17 -0300

    👷 Extract Kover coverage badge to README
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- .github/workflows/kover.main.kts
+++ .github/workflows/kover.main.kts
@@ -6,13 +6,16 @@
 @file:DependsOn("actions:checkout:v4")
 @file:DependsOn("actions:setup-java:v4")
 @file:DependsOn("gradle:gradle-build-action:v3")
+@file:DependsOn("peaceiris:actions-gh-pages:v3")
+
 
 import io.github.typesafegithub.workflows.actions.actions.Checkout
 import io.github.typesafegithub.workflows.actions.actions.SetupJava
 import io.github.typesafegithub.workflows.actions.gradle.GradleBuildAction
+import io.github.typesafegithub.workflows.actions.peaceiris.ActionsGhPages
 import io.github.typesafegithub.workflows.domain.RunnerType
-import io.github.typesafegithub.workflows.domain.triggers.PullRequest
 import io.github.typesafegithub.workflows.domain.triggers.Push
+import io.github.typesafegithub.workflows.dsl.expressions.expr
 import io.github.typesafegithub.workflows.dsl.workflow
 
 
@@ -26,5 +29,46 @@ workflow(
     uses(action = Checkout())
     uses(action = GradleBuildAction(arguments = "koverHtmlReport"))
     run(command = "cat app/build/reports/kover/html/index.html >> \$GITHUB_STEP_SUMMARY")
+
+    uses(action = GradleBuildAction(arguments = "koverXmlReport"))
+    run(
+      name = "Generate coverage output",
+      command = """
+                COVERAGE=$(${expr { github.workspace }}/gradlew -q printLineCoverage)
+                echo "Raw Coverage: ${'$'}COVERAGE"
+                COVERAGE=${'$'}(printf "%.0f" "${'$'}COVERAGE")  # Round to integer
+                echo "Rounded Coverage: ${'$'}COVERAGE"
+                echo "COVERAGE=${'$'}COVERAGE" >> ${'$'}GITHUB_ENV
+            """.trimIndent()
+    )
+
+    run(
+      name = "Generate Coverage Badge",
+      command = """
+                mkdir -p badge
+                COLOR=$(
+                    if [ "${'$'}COVERAGE" -ge 90 ]; then
+                        echo "brightgreen"
+                    elif [ "${'$'}COVERAGE" -ge 70 ]; then
+                        echo "yellow"
+                    else
+                        echo "red"
+                    fi
+                )
+                URL="https://img.shields.io/badge/Coverage-${'$'}COVERAGE%25-${'$'}COLOR"
+                curl -sS "${'$'}URL" -o badge/coverage-badge.svg
+            """.trimIndent()
+    )
+
+    uses(
+      name = "Deploy Badge to GitHub Pages",
+      action = ActionsGhPages(
+        githubToken = expr { secrets.GITHUB_TOKEN },
+        publishDir = "badge",
+        publishBranch = "gh-pages",
+        allowEmptyCommit = false,
+        forceOrphan = true
+      )
+    )
   }
 }
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@
  */
 
 import java.util.Properties
+import javax.xml.parsers.DocumentBuilderFactory
 import org.gradle.api.JavaVersion.VERSION_17
 
 @Suppress("DSL_SCOPE_VIOLATION") //KTIJ-19369
@@ -278,5 +279,42 @@ licensee {
 
   allowUrl("https://developer.android.com/guide/playcore/license") {
     because("Google Play License Agreement.")
+  }
+}
+
+/**
+ * Parse kover coverage report for badge creation
+ */
+tasks.register("printLineCoverage") {
+  group = "verification" // Put into the same group as the `kover` tasks
+  dependsOn("koverXmlReport")
+  doLast {
+    val report = file("${layout.buildDirectory.get()}/reports/kover/report.xml")
+
+    val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(report)
+    val rootNode = doc.firstChild
+    var childNode = rootNode.firstChild
+
+    var coveragePercent = 0.0
+
+    while (childNode != null) {
+      if (childNode.nodeName == "counter") {
+        val typeAttr = childNode.attributes.getNamedItem("type")
+        if (typeAttr.textContent == "LINE") {
+          val missedAttr = childNode.attributes.getNamedItem("missed")
+          val coveredAttr = childNode.attributes.getNamedItem("covered")
+
+          val missed = missedAttr.textContent.toLong()
+          val covered = coveredAttr.textContent.toLong()
+
+          coveragePercent = (covered * 100.0) / (missed + covered)
+
+          break
+        }
+      }
+      childNode = childNode.nextSibling
+    }
+
+    println("%.1f".format(coveragePercent))
   }
 }
