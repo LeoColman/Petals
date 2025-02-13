4053c4ccd94839a409e3cda1911b3b549d7ac442
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2025-02-13 17:46:33 -0300

    ♻️ Refactor Kover Coverage Collection
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- .github/workflows/kover.main.kts
+++ .github/workflows/kover.main.kts
@@ -1,17 +1,20 @@
 #!/usr/bin/env kotlin
 @file:Repository("https://repo1.maven.org/maven2/")
+@file:CompilerOptions("-Xmulti-dollar-interpolation")
 @file:DependsOn("io.github.typesafegithub:github-workflows-kt:3.2.0")
 
 @file:Repository("https://bindings.krzeminski.it")
 @file:DependsOn("actions:checkout:v4")
 @file:DependsOn("actions:setup-java:v4")
-@file:DependsOn("gradle:gradle-build-action:v3")
+@file:DependsOn("actions:upload-artifact:v4")
+@file:DependsOn("gradle:actions__setup-gradle:v3")
 @file:DependsOn("peaceiris:actions-gh-pages:v4")
 
 
 import io.github.typesafegithub.workflows.actions.actions.Checkout
 import io.github.typesafegithub.workflows.actions.actions.SetupJava
-import io.github.typesafegithub.workflows.actions.gradle.GradleBuildAction
+import io.github.typesafegithub.workflows.actions.actions.UploadArtifact
+import io.github.typesafegithub.workflows.actions.gradle.ActionsSetupGradle
 import io.github.typesafegithub.workflows.actions.peaceiris.ActionsGhPages
 import io.github.typesafegithub.workflows.domain.RunnerType
 import io.github.typesafegithub.workflows.domain.triggers.Push
@@ -21,50 +24,59 @@ import io.github.typesafegithub.workflows.dsl.workflow
 
 workflow(
   name = "Kover Coverage",
-  on = listOf(Push(branches = listOf("main"))),
+  on = listOf(Push(branches = listOf("main", "tweak-gha"))),
   sourceFile = __FILE__
 ) {
   job(id = "analyse", runsOn = RunnerType.UbuntuLatest) {
     uses(name = "Set up JDK", action = SetupJava(javaVersion = "17", distribution = SetupJava.Distribution.Adopt))
-    uses(action = Checkout())
-    uses(action = GradleBuildAction(arguments = "koverHtmlReport"))
-    run(command = "cat app/build/reports/kover/html/index.html >> \$GITHUB_STEP_SUMMARY")
-
-    uses(action = GradleBuildAction(arguments = "koverXmlReport"))
+    uses(name = "Checkout", action = Checkout())
+    uses(name = "Generate Kover Reports", action = ActionsSetupGradle(arguments = "koverHtmlReport koverXmlReport"))
+    uses(
+      name = "Upload Kover Reports",
+      action = UploadArtifact(name = "coverage-reports", path = listOf("app/build/reports/kover"))
+    )
     run(
-      name = "Generate coverage output",
-      command = """
-                COVERAGE=$(${expr { github.workspace }}/gradlew -q printLineCoverage)
-                echo "Raw Coverage: ${'$'}COVERAGE"
-                COVERAGE=${'$'}(printf "%.0f" "${'$'}COVERAGE")  # Round to integer
-                echo "Rounded Coverage: ${'$'}COVERAGE"
-                echo "COVERAGE=${'$'}COVERAGE" >> ${'$'}GITHUB_ENV
+      name = "Calculate Coverage",
+      command = $$"""
+                COVERAGE=$($${expr { github.workspace }}/gradlew -q printLineCoverage)
+                echo "Raw Coverage: $COVERAGE"
+                COVERAGE=$(printf "%.0f" "$COVERAGE")  # Round to integer
+                echo "Rounded Coverage: $COVERAGE"
+                echo "COVERAGE=$COVERAGE" >> $GITHUB_ENV
             """.trimIndent()
+    )
+
+    run(
+      name = "Update GitHub Summary",
+      command = $$"""
+        echo "## Code Coverage Report" >> $GITHUB_STEP_SUMMARY
+        echo "**Total Coverage:** $COVERAGE%" >> $GITHUB_STEP_SUMMARY
+      """.trimIndent()
     )
 
     run(
       name = "Generate Coverage Badge",
-      command = """
-                mkdir -p badge
-                COLOR=$(
-                    if [ "${'$'}COVERAGE" -ge 90 ]; then
-                        echo "brightgreen"
-                    elif [ "${'$'}COVERAGE" -ge 70 ]; then
-                        echo "yellow"
-                    else
-                        echo "red"
-                    fi
-                )
-                URL="https://img.shields.io/badge/Coverage-${'$'}COVERAGE%25-${'$'}COLOR"
-                curl -sS "${'$'}URL" -o badge/coverage-badge.svg
-            """.trimIndent()
+      command = $$"""
+        mkdir -p badge
+        COLOR=$(
+          if [ "$COVERAGE" -ge 90 ]; then
+            echo "brightgreen"
+          elif [ "$COVERAGE" -ge 70 ]; then
+            echo "yellow"
+          else
+            echo "red"
+          fi
+        )
+        URL="https://img.shields.io/badge/Coverage-$COVERAGE%25-$COLOR"
+        curl -sS "$URL" -o badge/coverage-badge.svg
+      """.trimIndent()
     )
 
     uses(
-      name = "Deploy Badge to GitHub Pages",
+      name = "Deploy Coverage Report & Badge",
       action = ActionsGhPages(
-        githubToken = expr { secrets.GITHUB_TOKEN },
-        publishDir = "badge",
+        githubToken = expr { "secrets.GITHUB_TOKEN" },
+        publishDir = "app/build/reports/kover/html",
         publishBranch = "gh-pages",
         allowEmptyCommit = false,
         forceOrphan = true
