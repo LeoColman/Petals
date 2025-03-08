f54fc5392abfc9865b98b158a3211baeb801732d
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2025-03-08 15:57:35 -0300

    ⚗️ Experiment with Android Tests
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

---
+++ .github/workflows/android-tests.main.kts
@@ -0,0 +1,40 @@
+#!/usr/bin/env kotlin
+@file:Repository("https://repo1.maven.org/maven2/")
+@file:DependsOn("io.github.typesafegithub:github-workflows-kt:3.2.0")
+
+@file:Repository("https://bindings.krzeminski.it")
+@file:DependsOn("actions:checkout:v4")
+@file:DependsOn("actions:setup-java:v4")
+@file:DependsOn("gradle:actions__setup-gradle:v4")
+@file:DependsOn("ReactiveCircus:android-emulator-runner:v2")
+
+import io.github.typesafegithub.workflows.actions.actions.Checkout
+import io.github.typesafegithub.workflows.actions.actions.SetupJava
+import io.github.typesafegithub.workflows.actions.gradle.ActionsSetupGradle
+import io.github.typesafegithub.workflows.actions.reactivecircus.AndroidEmulatorRunner
+import io.github.typesafegithub.workflows.domain.RunnerType
+import io.github.typesafegithub.workflows.domain.triggers.PullRequest
+import io.github.typesafegithub.workflows.domain.triggers.Push
+import io.github.typesafegithub.workflows.dsl.workflow
+
+
+workflow(
+  name = "Android Tests",
+  on = listOf(Push(branches = listOf("main")), PullRequest()),
+  sourceFile = __FILE__
+) {
+  job(id = "android-test", runsOn = RunnerType.UbuntuLatest) {
+    uses(name = "Setup JDK", action = SetupJava(javaVersion = "17", distribution = SetupJava.Distribution.Adopt))
+    uses(name = "Checkout", action = Checkout())
+    uses(name = "Setup Gradle", action = ActionsSetupGradle())
+    uses(
+      name = "Android Tests",
+      action = AndroidEmulatorRunner(
+        apiLevel = 33,
+        emulatorOptions = "-no-snapshot -no-audio -no-boot-anim",
+        disableAnimations = true,
+        script = "./gradlew connectedAndroidTest"
+      )
+    )
+  }
+}
