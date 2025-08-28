aa39e3f94e14b673011c654a71f7cab2514f76de
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2025-08-28 17:35:20 -0300

    ⬆️ Upgrade Kotest to 6.0.1
    Replace deprecated `forAll` with `withData` to align with new
    testing practices. Adjust configurations and tests accordingly
    for compatibility with the updated library.
    
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

--- app/src/test/kotlin/br/com/colman/petals/statistics/graph/formatter/DaysSinceFirstUseFormatterTest.kt
+++ app/src/test/kotlin/br/com/colman/petals/statistics/graph/formatter/DaysSinceFirstUseFormatterTest.kt
@@ -2,8 +2,7 @@ package br.com.colman.petals.statistics.graph.formatter
 
 import br.com.colman.petals.use.repository.Use
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.data.Row2
-import io.kotest.data.forAll
+import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 import java.time.LocalDateTime
 import java.time.Month
@@ -16,13 +15,13 @@ class DaysSinceFirstUseFormatterTest : FunSpec({
     Use(LocalDateTime.of(2021, Month.FEBRUARY, 9, 9, 30), 0.6.toBigDecimal(), 18.toBigDecimal())
   )
 
-  test("getFormattedValue returns correctly formatted days since first use (yyyy-MM-dd)") {
-    forAll(
-      Row2(0f, "2021-02-06"),
-      Row2(1f, "2021-02-07"),
-      Row2(2f, "2021-02-08"),
-      Row2(3f, "2021-02-09")
-    ) { value, expectedDay ->
+  context("getFormattedValue returns correctly formatted days since first use (yyyy-MM-dd)") {
+    withData(
+      Pair(0f, "2021-02-06"),
+      Pair(1f, "2021-02-07"),
+      Pair(2f, "2021-02-08"),
+      Pair(3f, "2021-02-09")
+    ) { (value, expectedDay) ->
       val actual = DaysSinceFirstUseFormatter(uses, "yyyy-MM-dd").formatDate.getFormattedValue(value, null)
       actual shouldBe expectedDay
     }
