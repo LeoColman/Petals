90a268344ae6a7197bb020645154da4cbaba51e0
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2026-01-10 17:34:46 -0300

    Initial Drug Test Page
    Signed-off-by: Leonardo Colman Lopes <dev@leonardo.colman.com.br>

---
+++ app/src/main/kotlin/br/com/colman/petals/drugtest/DrugTestPage.kt
@@ -0,0 +1,45 @@
+/*
+ * Petals APP
+ * Copyright (C) 2026 Leonardo Colman Lopes
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package br.com.colman.petals.drugtest
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import br.com.colman.petals.R
+
+@Composable
+fun DrugTestPage() {
+  Box(
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(16.dp)
+      .testTag("DrugTestPage"),
+    contentAlignment = Alignment.Center
+  ) {
+    Text(text = stringResource(R.string.drug_test_placeholder))
+  }
+}
